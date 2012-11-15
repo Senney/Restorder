394667833a6a5d95e2b9a087679a8f995f9790b2
Sean Heintz <sean.heintz@gmail.com>
2012-11-15 11:51:12 -0700

    Updates to Menu and MenuItem structures.
    Back end code.

---
+++ Restorder/Menu.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Restorder
+{
+    class Menu
+    {
+        private Dictionary<string, List<MenuItem>> m_menu;
+        public Dictionary<string, List<MenuItem>> MenuDict
+        {
+            get { return m_menu; }
+        }
+
+        public Menu()
+        {
+            m_menu = new Dictionary<string, List<MenuItem>>();
+        }
+
+        public void addItem(string category, MenuItem item)
+        {
+            if (!m_menu.ContainsKey(category))
+                m_menu[category] = new List<MenuItem>();
+
+            m_menu[category].Add(item);
+        }
+
+        public List<MenuItem> getItems(string category)
+        {
+            return m_menu[category];
+        }
+
+        public MenuItem getItem(string name)
+        {
+            // Loop through all items and find the specific item.
+            foreach (KeyValuePair<string, List<MenuItem>> entry in m_menu)
+            {
+                foreach (MenuItem k in entry.Value)
+                {
+                    if (k.Name == name)
+                        return k;
+                }
+            }
+
+            return null;
+        }
+    }
+}
--- Restorder/MenuItem.cs
+++ Restorder/MenuItem.cs
@@ -3,18 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using System.Drawing;
+
 namespace Restorder
 {
     class MenuItem
     {
         private string itemName;
-        private float itemCost;
-
-        List<string> Ingredients;
-
-        public MenuItem(string name, float cost, string[] ingredients = null)
+        public string Name
         {
+            get { return this.itemName; }
+            set { this.itemName = value; }
+        }
 
+        private float itemCost;
+        public float Cost
+        {
+            get { return this.itemCost; }
+            set { this.itemCost = value; }
+        }
+
+        private List<string> itemIngredients;
+        public List<string> Ingredients
+        {
+            get { return itemIngredients; }
+        }
+
+        private Image itemPicture;
+        public Image Picture
+        {
+            get { return itemPicture; }
+        }
+
+        public MenuItem(string name, float cost, string picture = null, string[] ingredients = null)
+        {
+            this.itemName = name;
+            this.itemCost = cost;
+
+            if (picture != null)
+            {
+                if (System.IO.File.Exists(picture))
+                    this.itemPicture = Image.FromFile(picture);
+                else
+                    throw new System.IO.FileNotFoundException("Could not locate image: " + picture);
+            }
+
+            this.itemIngredients = new List<string>();
+
+            if (ingredients != null)
+                this.itemIngredients.AddRange(ingredients);
         }
     }
 }
